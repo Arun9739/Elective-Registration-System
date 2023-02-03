423491b7fb0c7f6162600fce878fde918a555ff1
Arunchandra Halemani <79678417+Hunking9797@users.noreply.github.com>
2023-02-03 21:17:16 +0530

    Add files via upload

---
+++ Elective Course Registration JDBC/src/ConnectionProvider.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+
+    static Connection con;
+
+    public static Connection createConnection(){
+        try {
+            // loading the driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //creating connection
+            String USER = "root";
+            String PASSWORD = "12345";
+            String URL = "jdbc:mysql://localhost:3306/elective_registration_jdbc";
+
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
