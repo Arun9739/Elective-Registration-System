d85220fef86e960ca449df7b1f91b514f37973a7
Arunchandra Halemani <79678417+Hunking9797@users.noreply.github.com>
2023-02-03 21:15:50 +0530

    Add files via upload

--- Elective Course Registration JDBC/src/ECR.java
+++ Elective Course Registration JDBC/src/ECR.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import com.elective.registration.ConnectionProvider;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.util.regex.Pattern;
 
 class MyFrame extends JFrame{
 
@@ -85,23 +85,39 @@ class MyFrame extends JFrame{
         String AEpelective[] = {"None","Control Engineering", "Finite Element Analysis", "Fracture Mechanics", "Turbo Machines", "Aircraft System and Instrumentation"};
         String MEpelective[] = {"None","Theory of Elasticity", "Mechanics of Composite Materials","Energy Engineering", "IC Engines", "Industrial Engineering and Ergonomics","Non-Traditional Machining","Machine Drawing"};
         String CEpelective[] = {"None","Theory of Elasticity", "Open Channel Hydraulics", "Railways, Airports and Harbours", "Wastewater Engineering", "Alternative Building Materials"};
+        
+        String notCSE[] = {"None","ECE","EEE","AE","ME","CE"};
+        String notISE[] = notCSE;
+        String notECE[] = {"None","CSE","ISE","EEE","AE","ME","CE"};
+        String notEEE[] = {"None","CSE","ISE","ECE","AE","ME","CE"};
+        String notAE[] = {"None","CSE","ISE","ECE","EEE","ME","CE"};
+        String notME[] = {"None","CSE","ISE","ECE","EEE","AE","CE"};
+        String notCE[] = {"None","CSE","ISE","ECE","EEE","AE","ME"};
+        
         sdept.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 String selectedOption = (String) sdept.getSelectedItem();
                 switch(selectedOption){
                     case "Computer Science and Engineering": spelective.setModel(new DefaultComboBoxModel<>(CSEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notCSE));
                     break;
                     case "Information Science and Engineering": spelective.setModel(new DefaultComboBoxModel<>(ISEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notISE));
                     break;
                     case "Electronics and Communication Engineering": spelective.setModel(new DefaultComboBoxModel<>(ECpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notECE));
                     break;
                     case "Electrical and Electronics Engineering": spelective.setModel(new DefaultComboBoxModel<>(EEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notEEE));
                     break;
                     case "Aeronautical Engineering": spelective.setModel(new DefaultComboBoxModel<>(AEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notAE));
                     break;
                     case "Mechanical Engineering": spelective.setModel(new DefaultComboBoxModel<>(MEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notME));
                     break;
                     case "Civil Engineering": spelective.setModel(new DefaultComboBoxModel<>(CEpelective));
+                                                             sodept.setModel(new DefaultComboBoxModel<>(notCE));
                 }
             }
         });
@@ -119,7 +135,9 @@ class MyFrame extends JFrame{
         oelective.setBounds(20,400,200,20);
         oelective.setFont(new Font("Consolas", Font.BOLD, 20));
         c.add(oelective);
+        
         String sodepts[] = {"None","CSE","ISE","ECE","EEE","AE","ME","CE"};
+        
         sodept = new JComboBox<>(sodepts);
         sodept.setFont(new Font("Consolas", Font.BOLD, 15));
         sodept.setBounds(250,395,60,20);
@@ -128,7 +146,7 @@ class MyFrame extends JFrame{
         soelective.setFont(new Font("Consolas", Font.BOLD, 15));
         soelective.setBounds(310,395,300,20);
         c.add(soelective);
-
+        
         // Dynamic Open Elective Implementation
         String CSEoelective[] = {"None","NoSQL","Enterprise Resource Planning","Project Management","Principles of Cyber Security"};
         String ISEoelective[] = CSEoelective;
@@ -137,6 +155,7 @@ class MyFrame extends JFrame{
         String AEoelective[] = {"None","Wind Tunnel Techniques","Measurement and Metrology","Composite Materials"};
         String MEoelective[] = {"None","Product Design & Rapid Prototyping", "Automotive Engg. and Current Tech.","Mechatronics","Statistics for Engineers"};
         String CEoelective[] = {"None","Environmental Impace Assesment","Road Safety"};
+        
         sodept.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 String selectedOption = (String) sodept.getSelectedItem();
@@ -180,36 +199,57 @@ class MyFrame extends JFrame{
                     PreparedStatement pstmt = con.prepareStatement(submit_query);
                     
                     //set the values of parameter
-                    pstmt.setString(1, usn);
-                    pstmt.setString(1, name);
-                    pstmt.setString(1, phone);
-                    pstmt.setString(1, dept);
-                    pstmt.setString(1, gender);
-                    pstmt.setString(1, professionalElective);
-                    pstmt.setString(1, openElective);
-                    
-                    //execute
-                    pstmt.executeUpdate();
+                    if(Pattern.matches("[a-zA-Z]+", name)== true && usn != "" && name != "" && phone != "" && dept != "None" && gender != "None" && professionalElective != "None" && openElective != "None"){
+                        pstmt.setString(1, usn);
+                        pstmt.setString(2, name);
+                        pstmt.setString(3, phone);
+                        pstmt.setString(4, dept);
+                        pstmt.setString(5, gender);
+                        pstmt.setString(6, professionalElective);
+                        pstmt.setString(7, openElective);
 
-                    System.out.println("Added succesfully!!");
+                        //execute
+                        pstmt.executeUpdate();
+
+                        System.out.println("Added succesfully!!");
+                    }
+                    else{
+                        System.out.println("Fields can't be empty!");
+                    }
 
                 } catch (Exception except) {
                     except.printStackTrace();
                 }
-
-                System.out.println(name);
-                System.out.println(usn);
-                System.out.println(phone);
-                System.out.println(dept);
-                System.out.println(gender);
-                System.out.println(professionalElective);
-                System.out.println(openElective);
             }
         });
 
         delete = new JButton("Delete");
         delete.setBounds(150,450,100,30);
         c.add(delete);
+        
+        delete.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                try{
+                    Connection con = ConnectionProvider.createConnection(); 
+                    String delete_query = "delete from Student where susn = ?";
+
+                    //preparedStatement
+                    PreparedStatement pstmt = con.prepareStatement(delete_query);
+                    String toDeleteUSN = (String)susn.getText();
+                    pstmt.setString(1, toDeleteUSN);
+                    
+                    //execute
+                    pstmt.executeUpdate();
+                    
+                    System.out.println("Deleted Succesfully!");
+                        
+                }
+                catch(Exception except){
+                    except.printStackTrace();
+                }
+                
+            }
+        });
 
         reset = new JButton("Reset");
         reset.setBounds(280,450,100,30);
@@ -227,14 +267,13 @@ class MyFrame extends JFrame{
                 sodept.setSelectedItem("None");
             }
         });
-
         setVisible(true);
     }
 
 }
 
 public class ECR {
-    public static void main(String[] args) throws Exception {
-        MyFrame frame = new MyFrame();
+    public static void main(String[] args) {
+         MyFrame frame = new MyFrame();
     }
 }
